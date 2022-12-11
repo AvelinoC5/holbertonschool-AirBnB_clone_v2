966d21c472dfad21233781e42d0263dbb124b07c
AvelinoC5 <avelino_63@hotmail.com>
2022-12-11 12:07:34 -0500

    update file

--- models/place.py
+++ models/place.py
@@ -23,6 +23,8 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
+    reviews = relationship("Review", backref="place", cascade="delete")
+
     amenity_ids = []
 
     def __init__(self, *args, **kwargs):
